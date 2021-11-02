c616604f75f9c0ddfa1ac19b513af10fc2d6e60b
Junior Krigger <juniorkrigger@hotmail.com>
2021-11-02 12:51:55 -0300

    FizzBuzz

---
+++ Fizz.js
@@ -0,0 +1,15 @@
+
+const resultado =  fizzBuzz (5);
+    console.log(resultado);
+
+    function fizzBuzz(entrada){
+        if (typeof entrada !== 'number')
+            return 'Não é um número';
+        if (entrada % 3 === 0  && entrada % 5 === 0)
+            return 'FizBuzz'
+        if (entrada % 3 === 0)
+            return 'Fizz'
+        if (entrada % 5 === 0)
+            return 'Buzz'
+        return entrada;
+    }
